36786e33dae98a77182951c8816a8c83a674a5bf
hyperst8 <huypn82@gmail.com>
2025-04-28 14:47:41 +0200

    Create contact page

---
+++ src/app/contact-form/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BackButton from "@/components/BackButton/BackButton";
+
+const ContactPage = () => {
+  return (
+    <div className="titlePage">
+      <div className="titleContainer">
+        <BackButton />
+        <h1>Contact us</h1>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
