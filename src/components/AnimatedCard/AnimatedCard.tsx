47355e1551151135458586c4079840a621ac51de
hyperst8 <huypn82@gmail.com>
2025-04-28 22:27:12 +0200

    Add zoom, expanded function and styles

--- src/components/AnimatedCard/AnimatedCard.tsx
+++ src/components/AnimatedCard/AnimatedCard.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./animatedCard.module.css";
 
 interface AnimatedCardProps {
@@ -14,15 +15,51 @@ const AnimatedCard = ({
   text,
   authorName,
 }: AnimatedCardProps) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const handleExpanding = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className={styles.animatedCard}>
-      <div className={styles.imageWrapper}>
+    <div
+      className={`${styles.animatedCard} ${isExpanded ? styles.expanded : ""}`}
+    >
+      {isExpanded && (
+        <button
+          aria-label="close"
+          className={styles.closeBtn}
+          onClick={handleExpanding}
+        >
+          X
+        </button>
+      )}
+
+      <div className={styles.imageWrapper} onClick={handleExpanding}>
         <img src={imageUrl} alt={title} className={styles.cardImage} />
       </div>
+
       <div className={styles.content}>
         <h2 className={styles.title}>{title}</h2>
         <p className={styles.text}>{text}</p>
+        <button
+          className={styles.zoomBtn}
+          onClick={handleExpanding}
+          aria-label="zoom"
+        >
+          <div className={styles.icon}>
+            <span className={`${styles.corner} ${styles.topLeft}`} />
+            <span className={`${styles.corner} ${styles.bottomRight}`} />
+          </div>
+        </button>
       </div>
+
+      {isExpanded && (
+        <div className={styles.expandedInfo}>
+          <h2 className={styles.expandedTitle}>{title}</h2>
+          <p className={styles.author}>By {authorName}</p>
+        </div>
+      )}
     </div>
   );
 };
